c251c3fc5cd4710599cd5aec4ba45c4966cdae57
formula38 <louismconley@gmail.com>
2022-11-16 09:59:08 -0800

    Initial Commit

---
+++ src/main/java/com/infosystraining/novelcharacterapp/NovelcharacterappApplication.java
@@ -0,0 +1,13 @@
+package com.infosystraining.novelcharacterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelcharacterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelcharacterappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosystraining/novelcharacterapp/NovelcharacterappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosystraining.novelcharacterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelcharacterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
