25c5b470ebe9ca9a13a53a94f0569c0f8bea01a9
formula38 <louismconley@gmail.com>
2022-11-16 11:19:11 -0800

    Setting Novel and FictionalCharacter model classes

--- src/main/java/com/infosystraining/novelcharacterapp/model/FictionalCharacter.java
+++ src/main/java/com/infosystraining/novelcharacterapp/model/FictionalCharacter.java
@@ -2,15 +2,19 @@ package com.infosystraining.novelcharacterapp.model;
 
 import org.hibernate.type.CharacterType;
 
+import javax.persistence.CascadeType;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
 
 public class FictionalCharacter {
+    private Long id;
     private String name;
     private String age;
-    private String location;
     @Enumerated(EnumType.STRING)
     private Gender gender;
     @Enumerated(EnumType.STRING)
     private CharacterRole role;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Novel novel;
 }
--- src/main/java/com/infosystraining/novelcharacterapp/model/Novel.java
+++ src/main/java/com/infosystraining/novelcharacterapp/model/Novel.java
@@ -1,6 +1,7 @@
 package com.infosystraining.novelcharacterapp.model;
 
 public class Novel {
+    private Long id;
     private String title;
     private String author;
     private String publisher;
