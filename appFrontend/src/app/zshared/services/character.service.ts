3f9631900d323cfee3bb18e028d5f4e0df63ecd1
formula38 <louismconley@gmail.com>
2022-11-28 17:20:19 -0800

    Character service with basic CRUD methods

--- appFrontend/src/app/zshared/services/character.service.ts
+++ appFrontend/src/app/zshared/services/character.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Character} from "../models/character.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,30 @@ export class CharacterService {
   private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
+
+  public getCharacters(): Observable<Character[]> {
+    return this.http
+      .get<Character[]>(`${this.apiServerUrl}/v1/characters/all`);
+  }
+
+  public getCharacter(characterId: number): Observable<Character> {
+    return this.http
+      .get<Character>(`${this.apiServerUrl}/v1/characters/find${characterId}`);
+  }
+
+  public addCharacter(newCharacter: Character): Observable<Character> {
+    return this.http
+      .post<Character>(`${this.apiServerUrl}/v1/characters/add`, newCharacter);
+  }
+
+  public updateCharacter(character: Character): Observable<Character> {
+    return this.http
+      .put<Character>(`${this.apiServerUrl}/v1/characters/update`, character);
+  }
+
+  public deleteCharacter(characterId: number): Observable<void> {
+    return this.http
+      .delete<void>(`${this.apiServerUrl}/v1/characters/delete${characterId}`);
+  }
 }
 
