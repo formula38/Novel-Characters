9b50a9deb5bca45ac31f1846b13fb0a00d9c5a40
formula38 <louismconley@gmail.com>
2022-11-28 16:54:13 -0800

    Created frontend novel and character interface models

---
+++ appFrontend/src/app/zshared/models/character.model.ts
@@ -0,0 +1,7 @@
+export interface CharacterModel {
+  id: number;
+  name: string;
+  age: string;
+  gender: string;
+  role: string;
+}
---
+++ appFrontend/src/app/zshared/models/novel.model.ts
@@ -0,0 +1,5 @@
+export interface NovelModel {
+  id: number;
+  title: string;
+  author: string;
+}
